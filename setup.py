2859c814f0960b3e361e024ff63995a3e44ffcad
Jeroen Overschie <jeroenoverschie@gmail.com>
2021-04-26 15:00:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='chen-tabular-synthetic-datasets',
+    name='l2x_synthetic',
     version='1.0.0',
     description='4 simple customizable synthetic datasets from Chen et al., 2018 (L2X): Orange Skin, XOR, Non-linear Additive and Switch.',
     author='Jianbo Chen',
     url='https://github.com/dunnkers/L2X',
-    packages=find_packages(include=['synthetic', 'synthetic.*'])
+    packages=find_packages(include=['l2x_synthetic', 'l2x_synthetic.*'])
 )
