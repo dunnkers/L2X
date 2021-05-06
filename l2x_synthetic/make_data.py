f6b3708483013143dafb62d9e9d4c2d5fcccfa8d
Jeroen Overschie <jeroenoverschie@gmail.com>
2021-05-06 11:01:10 +0200

    Format code with black ✨

--- l2x_synthetic/make_data.py
+++ l2x_synthetic/make_data.py
@@ -2,109 +2,118 @@
 This script contains functions for generating synthetic data. 
 
 Part of the code is based on https://github.com/Jianbo-Lab/CCM
-""" 
+"""
 import numpy as np
 import pandas as pd
 
+
 def generate_XOR_labels(X):
-    y = np.exp(X[:,0]*X[:,1])
+    y = np.exp(X[:, 0] * X[:, 1])
 
-    prob_1 = np.expand_dims(1 / (1+y) ,1)
-    prob_0 = np.expand_dims(y / (1+y) ,1)
+    prob_1 = np.expand_dims(1 / (1 + y), 1)
+    prob_0 = np.expand_dims(y / (1 + y), 1)
 
-    y = np.concatenate((prob_0,prob_1), axis = 1)
+    y = np.concatenate((prob_0, prob_1), axis=1)
 
     return y
+
 
 def generate_orange_labels(X):
-    logit = np.exp(np.sum(X[:,:4]**2, axis = 1) - 4.0) 
+    logit = np.exp(np.sum(X[:, :4] ** 2, axis=1) - 4.0)
 
-    prob_1 = np.expand_dims(1 / (1+logit) ,1)
-    prob_0 = np.expand_dims(logit / (1+logit) ,1)
+    prob_1 = np.expand_dims(1 / (1 + logit), 1)
+    prob_0 = np.expand_dims(logit / (1 + logit), 1)
 
-    y = np.concatenate((prob_0,prob_1), axis = 1)
+    y = np.concatenate((prob_0, prob_1), axis=1)
 
     return y
+
 
 def generate_additive_labels(X):
-    logit = np.exp(-100 * np.sin(0.2*X[:,0]) + abs(X[:,1]) + X[:,2] + np.exp(-X[:,3])  - 2.4) 
+    logit = np.exp(
+        -100 * np.sin(0.2 * X[:, 0]) + abs(X[:, 1]) + X[:, 2] + np.exp(-X[:, 3]) - 2.4
+    )
 
-    prob_1 = np.expand_dims(1 / (1+logit) ,1)
-    prob_0 = np.expand_dims(logit / (1+logit) ,1)
+    prob_1 = np.expand_dims(1 / (1 + logit), 1)
+    prob_0 = np.expand_dims(logit / (1 + logit), 1)
 
-    y = np.concatenate((prob_0,prob_1), axis = 1)
+    y = np.concatenate((prob_0, prob_1), axis=1)
 
     return y
 
 
-
-def generate_data(n=100, datatype='', seed=0, as_frame=False):
+def generate_data(n=100, datatype="", seed=0, as_frame=False):
     """
     Generate data (X,y)
     Args:
-        n (int): number of samples 
-        datatype (str): The type of data 
+        n (int): number of samples
+        datatype (str): The type of data
             ('orange_skin' | 'XOR' | 'nonlinear_additive' | 'switch')
         seed (int): random seed used
         as_frame (bool): return data as DataFrame.
-    Return: 
-        X (float): [n, d].  
-        y (float): n dimensional array. 
+    Return:
+        X (float): [n, d].
+        y (float): n dimensional array.
         OR
         df (pd.core.DataFrame): DataFrame containing features with the column names
             {X1, ..., Xp} and targets {Y1, ..., Y2}
     """
-    assert datatype in ['orange_skin', 'XOR', 'nonlinear_additive', 'switch']
+    assert datatype in ["orange_skin", "XOR", "nonlinear_additive", "switch"]
 
     np.random.seed(seed)
 
     X = np.random.randn(n, 10)
 
     datatypes = None
-    if datatype == 'orange_skin': 
-        y = generate_orange_labels(X) 
+    if datatype == "orange_skin":
+        y = generate_orange_labels(X)
 
-    elif datatype == 'XOR':
-        y = generate_XOR_labels(X)    
+    elif datatype == "XOR":
+        y = generate_XOR_labels(X)
 
-    elif datatype == 'nonlinear_additive':  
-        y = generate_additive_labels(X) 
+    elif datatype == "nonlinear_additive":
+        y = generate_additive_labels(X)
 
-    elif datatype == 'switch':
+    elif datatype == "switch":
 
         # Construct X as a mixture of two Gaussians.
-        X[:n//2,-1] += 3
-        X[n//2:,-1] += -3
-        X1 = X[:n//2]; X2 = X[n//2:]
+        X[: n // 2, -1] += 3
+        X[n // 2 :, -1] += -3
+        # first n / 2 instances will be "Orange Skin"
+        X1 = X[: n // 2]
+        # last n / 2 instances will be "Additive Labels"
+        X2 = X[n // 2 :]
 
         y1 = generate_orange_labels(X1)
         y2 = generate_additive_labels(X2)
 
         # Set the key features of X2 to be the 4-8th features.
-        X2[:,4:8],X2[:,:4] = X2[:,:4],X2[:,4:8]
+        X2[:, 4:8], X2[:, :4] = X2[:, :4], X2[:, 4:8]
 
-        X = np.concatenate([X1,X2], axis = 0)
-        y = np.concatenate([y1,y2], axis = 0) 
+        X = np.concatenate([X1, X2], axis=0)
+        y = np.concatenate([y1, y2], axis=0)
 
         # Used for evaluation purposes.
-        datatypes = np.array(['orange_skin'] * len(y1) + ['nonlinear_additive'] * len(y2)) 
+        datatypes = np.array(
+            ["orange_skin"] * len(y1) + ["nonlinear_additive"] * len(y2)
+        )
 
         # Permute the instances randomly.
         perm_inds = np.random.permutation(n)
-        X,y = X[perm_inds],y[perm_inds]
+        X, y = X[perm_inds], y[perm_inds]
         datatypes = datatypes[perm_inds]
 
     if as_frame:
         n, p = X.shape
-        feature_names = [f'X{i}' for i in range(1, p + 1)]
+        feature_names = [f"X{i}" for i in range(1, p + 1)]
         features = pd.DataFrame(X, columns=feature_names)
 
         n, p = y.shape
-        target_names = [f'Y{i}' for i in range(1, p + 1)]
+        target_names = [f"Y{i}" for i in range(1, p + 1)]
         targets = pd.DataFrame(y, columns=target_names)
 
         df = features.join(targets)
-        df['datatypes'] = datatypes
+        df["datatypes"] = datatypes
 
         return df
     else:
--- l2x_synthetic/make_data_test.py
+++ l2x_synthetic/make_data_test.py
@@ -2,9 +2,10 @@ import unittest
 from make_data import generate_data
 import pandas as pd
 
+
 class TestMakeData(unittest.TestCase):
     def test_generate_data(self):
-        X, y = generate_data(n=100, datatype='XOR')
+        X, y = generate_data(n=100, datatype="XOR")
         n, p = X.shape
         assert n == 100
         assert p == 10
@@ -13,8 +14,9 @@ class TestMakeData(unittest.TestCase):
         assert p == 2
 
     def test_dataframe_output(self):
-        df = generate_data(n=100, datatype='XOR', as_frame=True)
+        df = generate_data(n=100, datatype="XOR", as_frame=True)
         assert type(df) == pd.DataFrame
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
